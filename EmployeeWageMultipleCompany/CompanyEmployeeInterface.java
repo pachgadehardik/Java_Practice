20620f1f7dbbc5e9d5edddfc626bc053d86b7f43
pachgadehardik <pachgadehardik@gmail.com>
2020-09-25 23:17:01 +0530

    Repo Cleanup

--- EmployeeWageMultipleCompany/CompanyEmployeeInterface.java
+++
@@ -1,6 +0,0 @@
-//Creating Interface for the common behavior of multiple companies
-public interface CompanyEmployeeInterface{
-	public void computeEmpWage();
-	public void addCompanyEmpData(String companyName, int Emp_Rate_per_hr, int Number_of_Working_Days, int Max_Working_Hrs);
-	public void computeEmpWage(CompanyEmployeeData companyEmployeeData);
-}
