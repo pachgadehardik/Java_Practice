d4ae9287174955315aae7c71910a15b52c32908a
pachgadehardik <pachgadehardik@gmail.com>
2020-09-25 23:11:30 +0530

    Added EmployeeWageComputation all 10 Usecases

---
+++ src/employeewagemultiple/CompanyEmployeeData.java
@@ -0,0 +1,22 @@
+package employeewagemultiple;
+
+//Creating a class for Setting of the Employee Data
+
+
+public class CompanyEmployeeData {
+
+	public String companyName;
+	public int Emp_Rate_per_hr;
+	public int Number_of_Working_Days;
+	int Max_Working_Hrs;
+	public int totalEmpWage;
+	
+	public CompanyEmployeeData(String companyName, int Emp_Rate_per_hr, int Number_of_Working_Days, int Max_Working_Hrs) {
+		// TODO Auto-generated constructor stub
+		this.companyName = companyName;
+		this.Emp_Rate_per_hr = Emp_Rate_per_hr;
+		this.Number_of_Working_Days = Number_of_Working_Days;
+		this.Max_Working_Hrs  = Max_Working_Hrs;
+		totalEmpWage = 0;
+	}
+}
