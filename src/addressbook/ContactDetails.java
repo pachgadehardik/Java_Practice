9f1bbca596867cec7afa4dd622438931a39c2066
pachgadehardik <pachgadehardik@gmail.com>
2020-09-25 23:09:02 +0530

    Added AddressBook System Usecase 1 to 5

---
+++ src/addressbook/ContactDetails.java
@@ -0,0 +1,68 @@
+package addressbook;
+
+public class ContactDetails {
+	
+	private String firstName, lastName, address, city, state, phone_no, zip, email;
+	private int id;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPhone_no() {
+		return phone_no;
+	}
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return "ID: "+ this.id+ "\nFirstName: "+ this.firstName+ "\n Lastname: "+this.lastName+ "\n Address: "+this.address+"\n City: "+this.city+"\n State: "+this.state+
+				"\n Zip: "+this.zip+"\n Phone Number: "+this.phone_no+"\n Email: "+this.email;
+	}
+	
+}
