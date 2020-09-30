dcecf5a200de56a8e789128e1614411c45a87494
pachgadehardik <pachgadehardik@gmail.com>
2020-09-30 11:27:20 +0530

    UserDetails Testing using Junit Completed for all the Testcases

--- src/uservalidationtest/RegexValidatorTest.java
+++ src/uservalidationtest/RegexValidatorTest.java
@@ -15,54 +15,40 @@ class RegexValidatorTest {
 
 	@Test
 	public void givenFirstLastName_WhenProper_ShouldReturnTrue() {
-			boolean result = RegexValidator.checkRegex(RegexValidator.uc1, "Hrdik");
-			boolean result2 = RegexValidator.checkRegex(RegexValidator.uc2, "Pachgade");
-			assertEquals(true, result);
-			assertEquals(true, result);
+			assertEquals(true, RegexValidator.checkRegex(RegexValidator.uc1, "Hrdik"));
+			assertEquals(true, RegexValidator.checkRegex(RegexValidator.uc2, "Pachgade"));
 	}
 	
 	@Test
 	public void givenFirstLastName_WhenShort_ShouldReturnFalse() {
-		boolean result1 = RegexValidator.checkRegex(RegexValidator.uc1, "ik");
-		boolean result2 = RegexValidator.checkRegex(RegexValidator.uc2, "pachgade");
-		assertEquals(false, result1);
-		assertEquals(false, result2);
+		assertEquals(false, RegexValidator.checkRegex(RegexValidator.uc1, "ik"));
+		assertEquals(false, RegexValidator.checkRegex(RegexValidator.uc2, "pachgade"));
 	}
 	
 	@Test
 	public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
-		boolean result = RegexValidator.checkRegex(RegexValidator.uc4, "91 9878998701");
-		assertEquals(true, result);
+		assertEquals(true, RegexValidator.checkRegex(RegexValidator.uc4, "91 9878998701"));
 	}
 	
 	@Test
 	public void givenEmail_WhenProper_ShouldReturnTrue() {
-		boolean result = RegexValidator.checkRegex(RegexValidator.uc3, "hardik.asd@asdf.co.in");
-		assertEquals(true, result);
+		assertEquals(true,  RegexValidator.checkRegex(RegexValidator.uc3, "hardik.asd@asdf.co.in"));
 	}
 	
 	@Test
 	public void givenMobileNumber_WhenLess_ShouldReturnFalse() {
-		boolean result = RegexValidator.checkRegex(RegexValidator.uc4, "91 98789901");
-		assertEquals(false, result);
+		assertEquals(false, RegexValidator.checkRegex(RegexValidator.uc4, "91 98789901"));
 	}
+	
 	
 	@Test
 	public void givenPassWord_WhenAllRulesProper_ShouldReturnTrue() {
-		boolean result1 = RegexValidator.checkRegex(RegexValidator.uc5, "hardikab");
-		boolean result2 = RegexValidator.checkRegex(RegexValidator.uc6,"Hardikab");
-		boolean result3 = RegexValidator.checkRegex(RegexValidator.uc7,"HardikAb12");
-		boolean result4 = RegexValidator.checkRegex(RegexValidator.uc8, "Hardik@12");
-		assertEquals(true, (result1 && result2 && result3 && result4));
+		assertEquals(true, (RegexValidator.checkRegex(RegexValidator.uc5, "hardikab") && RegexValidator.checkRegex(RegexValidator.uc6,"Hardikab") && RegexValidator.checkRegex(RegexValidator.uc7,"HardikAb12") && RegexValidator.checkRegex(RegexValidator.uc8, "Hardik@12")));
 	}
 	
 	@Test
 	public void givenPassWord_WhenAllRulesNotProper_ShouldReturnFalse() {
-		boolean result1 = RegexValidator.checkRegex(RegexValidator.uc5, "hardik");
-		boolean result2 = RegexValidator.checkRegex(RegexValidator.uc6,"hardikab");
-		boolean result3 = RegexValidator.checkRegex(RegexValidator.uc7,"HardikAb");
-		boolean result4 = RegexValidator.checkRegex(RegexValidator.uc8, "Hardik@@@12");
-		assertEquals(false, (result1 && result2 && result3 && result4));
+		assertEquals(false, (RegexValidator.checkRegex(RegexValidator.uc5, "hardik") && RegexValidator.checkRegex(RegexValidator.uc6,"hardikab") && RegexValidator.checkRegex(RegexValidator.uc7,"HardikAb") && RegexValidator.checkRegex(RegexValidator.uc8, "Hardik@@@12")));
 	}
 	
 	
